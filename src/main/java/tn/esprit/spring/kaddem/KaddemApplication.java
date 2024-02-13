229af6a1db9a28ab06a618439815030d2dcc8ccf
Phenix0103 <cyrine.chouchane@esprit.tn>
2024-02-13 01:02:43 +0100

    testing the jenkinsfile

--- src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
+++ src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class KaddemApplication {
-
+    //test jenkinsfile
     public static void main(String[] args) {
         SpringApplication.run(KaddemApplication.class, args);
     }
